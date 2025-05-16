399f85a52e786a723cc59117005a4050ca34ed0d
Name <vova.zest997@gmail.com>
2025-05-16 07:36:10 +0300

    placeholder

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,18 +1,23 @@
-import { Formik, Form, Field, ErrorMessage } from "formik";
-import css from "./SearchBar.module.css";
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import css from './SearchBar.module.css';
 
 export default function Searchbar({ onSearch }) {
   return (
     <header className={css.searchbar}>
       <Formik
-        initialValues={{ query: "" }}
+        initialValues={{ query: '' }}
         onSubmit={(values, actions) => {
           onSearch(values.query);
           actions.resetForm(); //reset
         }}
       >
         <Form className={css.form}>
-          <Field type="text" name="query" className={css.input} />
+          <Field
+            placeholder="Search images and photos"
+            type="text"
+            name="query"
+            className={css.input}
+          />
           <button type="submit" className={css.button}>
             Search
           </button>
